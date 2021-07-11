364b435298364e261c1c5a0170511ece3a98f313
Samy <200020040@iitdh.ac.in>
2021-07-11 16:37:34 +0530

    Deployed

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://coders-garage.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
