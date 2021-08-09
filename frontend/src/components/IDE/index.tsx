b284591398bebd0dbc3fb20f10721019a2f374ea
Samy <200020040@iitdh.ac.in>
2021-08-09 14:02:56 +0530

    Implemented Common IDE features except autocomplete

--- frontend/src/components/IDE/index.tsx
+++ frontend/src/components/IDE/index.tsx
@@ -6,6 +6,22 @@ import { SocketContext } from '../../config/SocketContext';
 import { useStyles } from './styles';
 import './styles.css';
 
+import 'codemirror/mode/clike/clike';
+import 'codemirror/mode/python/python';
+import 'codemirror/mode/ruby/ruby';
+import 'codemirror/mode/swift/swift';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/matchtags';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/comment/comment';
+import 'codemirror/addon/fold/foldcode';
+import 'codemirror/addon/fold/foldgutter';
+import 'codemirror/addon/fold/brace-fold';
+import 'codemirror/addon/fold/comment-fold';
+import 'codemirror/addon/fold/indent-fold';
+import 'codemirror/addon/selection/active-line';
+
 interface Iprops {
     value: any,
     onChange: any,
@@ -32,10 +48,23 @@ const Ide = ({ value, onChange, language, isLight }: Iprops) => {
                         onChange(value);
                     }}
                     options={{
+                        autofocus: true,
+                        lineWrapping: true,
                         mode: language,
                         theme: isLight?light:dark,
                         lineNumbers: true,
+                        extraKeys: {
+                            'Cmd-/' : 'toggleComment',
+                            'Ctrl-/' : 'toggleComment'
+                        },
                         autocorrect: true,
+                        matchBrackets: true,
+                        autoCloseBrackets: true,
+                        matchTags: true,
+                        autoCloseTags: true,
+                        foldGutter: true,
+                        gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+                        styleActiveLine: true
                     }}
                     autoCursor
                     autoScroll
@@ -66,10 +95,23 @@ export const InterviewIde = ({ value, onChange, language, isLight, interviewMode
                         sendChange(value);
                     }}
                     options={{
+                        autofocus: true,
+                        lineWrapping: true,
                         mode: language,
                         theme: isLight?light:dark,
                         lineNumbers: true,
+                        extraKeys: {
+                            'Cmd-/' : 'toggleComment',
+                            'Ctrl-/' : 'toggleComment'
+                        },
                         autocorrect: true,
+                        matchBrackets: true,
+                        autoCloseBrackets: true,
+                        matchTags: true,
+                        autoCloseTags: true,
+                        foldGutter: true,
+                        gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+                        styleActiveLine: true
                     }}
                     autoCursor
                     autoScroll
