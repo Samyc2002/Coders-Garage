1f7b1334a328952175253d8f2050ecc84b2160c3
Samriddha Chattopadhyay <200020040@iitdh.ac.in>
2022-05-01 01:58:34 +0530

    A commit after a long time

--- server/src/environments/dev.env.ts
+++ server/src/environments/dev.env.ts
@@ -1,6 +1,6 @@
 import { Environment } from "./env";
 
 export const DevEnvironment: Environment = {
-    db_url: 'mongodb+srv://CodersGarage:codersgarage@cluster0.hnqwh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    db_url: '',
     jwt_secret: 'secret',
 }
